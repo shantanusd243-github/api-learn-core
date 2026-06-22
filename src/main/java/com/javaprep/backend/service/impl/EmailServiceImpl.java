589947d1242e2f6c1470c50e9953ebce415b1f43
Shantanu Deshmukh <shantanu.sd243@gmail.com>
2026-06-22 12:20:48 +0530

    Email fix

--- src/main/java/com/javaprep/backend/service/impl/EmailServiceImpl.java
+++ src/main/java/com/javaprep/backend/service/impl/EmailServiceImpl.java
@@ -32,7 +32,7 @@ public class EmailServiceImpl implements EmailService {
     private String fromEmail;
 
     // Configurable app name (defaults to "Learnin Prep" if missing in application.yml)
-    @Value("${spring.application.application}")
+    @Value("${spring.application.name}")
     private String appName;
 
     @Async
