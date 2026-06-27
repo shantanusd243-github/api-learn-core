daa95f2363c0f1fd128c1539480726cebbcca205
Shantanu Deshmukh <shantanu.sd243@gmail.com>
2026-06-27 12:15:44 +0530

    fixed domain url

--- src/main/java/com/javaprep/backend/config/KeepAliveConfig.java
+++ src/main/java/com/javaprep/backend/config/KeepAliveConfig.java
@@ -1,6 +1,7 @@
 package com.javaprep.backend.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class KeepAliveConfig {
 
+    @Value("${spring.domain.url}")
+    private String applicationDomainUrl;
     // 840000 milliseconds = 14 minutes
     @Scheduled(fixedRate = 840000) 
     public void pingSelf() {
@@ -18,7 +21,7 @@ public class KeepAliveConfig {
             log.info("💓 Sending keep-alive ping to prevent Render free-tier sleep...");
             RestTemplate restTemplate = new RestTemplate();
             // Pings your public health endpoint
-            restTemplate.getForObject("https://api-learn-core.onrender.com/actuator/health", String.class);
+            restTemplate.getForObject(applicationDomainUrl + "/actuator/health", String.class);
             log.info("✅ Keep-alive ping successful.");
         } catch (Exception e) {
             log.warn("Keep-alive ping encountered an issue, but server is still awake.");
